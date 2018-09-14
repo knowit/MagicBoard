8a4985dc1b96f96f2caeea806b4cfe54eb392209
Kien Vu <kienv91@gmail.com>
2018-09-14 09:50:27 +0200

    updated to double qutoes

--- modules/MMM-MotionSensor/node_helper.js
+++ modules/MMM-MotionSensor/node_helper.js
@@ -1,28 +1,26 @@
-const NodeHelper = require('node_helper');
+const NodeHelper = require("node_helper");
 
-const SerialPort = require('serialport');
-const Readline = require('@serialport/parser-readline');
+const SerialPort = require("serialport");
+const Readline = require("@serialport/parser-readline");
 
-const port = new SerialPort('/dev/ttyUSB0', { baudRate: 9600 });
-const parser = port.pipe(new Readline({ delimiter: '\n' }));
+const port = new SerialPort("/dev/ttyUSB0", { baudRate: 9600 });
+const parser = port.pipe(new Readline({ delimiter: "\n" }));
 
 module.exports = NodeHelper.create({
-
   subscribe_motion: function() {
     var self = this;
-    parser.on('data', data => {
+    parser.on("data", data => {
       if (data == 1) {
-        self.sendSocketNotification('MOTION_DETECTED', data)
+        self.sendSocketNotification("MOTION_DETECTED", data);
       } else {
-        self.sendSocketNotification('NO_MOTION_DETECTED', data)
+        self.sendSocketNotification("NO_MOTION_DETECTED", data);
       }
-    }
-    )
+    });
   },
 
   // Subclass socketNotificationReceived received.
   socketNotificationReceived: function(notification, payload) {
-    if (notification === 'START_MOTION_DETECTION') {
+    if (notification === "START_MOTION_DETECTION") {
       this.subscribe_motion();
     }
   }
