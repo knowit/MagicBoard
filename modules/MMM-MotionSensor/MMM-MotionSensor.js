42e16f5cbb90ee3b429958b6956beb2edf9c2cd9
Kien Vu <kienv91@gmail.com>
2018-09-28 09:44:26 +0200

    added MMM-MotionSensor

---
+++ modules/MMM-MotionSensor/MMM-MotionSensor.js
@@ -0,0 +1,60 @@
+/* global Module */
+
+/* Motion Sensor
+ * Module: MMM-MotionSensor
+ *
+ * 
+ * MIT Licensed.
+ */
+
+Module.register("MMM-MotionSensor", {
+	defaults: {},
+
+	getStyles: function() {
+		return ["MMM-MotionSensor.css"];
+	},
+
+	start: function() {
+		Log.info("Starting module: " + this.name);
+		this.enableBlackScreen = false;
+		this.updateDom();
+		this.sendSocketNotification("START_MOTION_DETECTION", {});
+	},
+
+	// the socket handler
+	socketNotificationReceived: function(notification, payload) {
+		Log.log(
+			this.name +
+				" received a socket notification: " +
+				notification +
+				" - Payload: " +
+				payload,
+		);
+
+		if (notification === "MOTION_DETECTED" && this.enableBlackScreen) {
+			this.enableBlackScreen = false;
+			this.updateDom();
+		} else if (
+			notification === "NO_MOTION_DETECTED" &&
+			!this.enableBlackScreen
+		) {
+			this.enableBlackScreen = true;
+			this.updateDom();
+		}
+	},
+
+	getDom: function() {
+		var wrapper = document.getElementById("fullscreen-container");
+
+		if (this.enableBlackScreen) {
+			var childNode = wrapper.firstChild;
+			childNode.style.display = "none";
+
+			wrapper.style.display = "block";
+			wrapper.style.backgroundColor = "black";
+			wrapper.style.zIndex = 5000;
+		} else {
+			wrapper.style.display = "none";
+		}
+	},
+});
