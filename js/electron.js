d58b93419485b17f101bfa08cab8737ad0d5a68a
Kien Vu <kienv91@gmail.com>
2018-10-17 10:07:05 +0200

    fixed misc

--- js/electron.js
+++ js/electron.js
@@ -17,90 +17,109 @@ const BrowserWindow = electron.BrowserWindow;
 let mainWindow;
 
 function createWindow() {
-  var electronOptionsDefaults = {
-    width: 800,
-    height: 600,
-    x: 0,
-    y: 0,
-    darkTheme: true,
-    webPreferences: {
-      nodeIntegration: false,
-      zoomFactor: config.zoom,
-    },
-    backgroundColor: "#000000",
-  };
+	var electronOptionsDefaults = {
+		width: 800,
+		height: 600,
+		x: 0,
+		y: 0,
+		darkTheme: true,
+		webPreferences: {
+			nodeIntegration: false,
+			zoomFactor: config.zoom,
+		},
+		backgroundColor: "#000000",
+	};
 
-  // DEPRECATED: "kioskmode" backwards compatibility, to be removed
-  // settings these options directly instead provides cleaner interface
-  if (config.kioskmode) {
-    electronOptionsDefaults.kiosk = true;
-  } else {
-    electronOptionsDefaults.fullscreen = true;
-    electronOptionsDefaults.autoHideMenuBar = true;
-  }
+	// DEPRECATED: "kioskmode" backwards compatibility, to be removed
+	// settings these options directly instead provides cleaner interface
+	if (config.kioskmode) {
+		electronOptionsDefaults.kiosk = true;
+	} else {
+		electronOptionsDefaults.fullscreen = true;
+		electronOptionsDefaults.autoHideMenuBar = true;
+	}
 
-  var electronOptions = Object.assign(
-    {},
-    electronOptionsDefaults,
-    config.electronOptions,
-  );
+	var electronOptions = Object.assign(
+		{},
+		electronOptionsDefaults,
+		config.electronOptions,
+	);
 
-  // Create the browser window.
-  mainWindow = new BrowserWindow(electronOptions);
+	var myWindow = null;
 
-  // and load the index.html of the app.
-  // If config.address is not defined or is an empty string (listening on all interfaces), connect to localhost
-  var address =
-    (config.address === void 0) | (config.address === "")
-      ? (config.address = "localhost")
-      : config.address;
-  mainWindow.loadURL(`http://${address}:${config.port}`);
+	var shouldQuit = app.makeSingleInstance(function(
+		commandLine,
+		workingDirectory,
+	) {
+		if (myWindow) {
+			if (myWindow.isMinimized()) myWindow.restore();
+			myWindow.focus();
+		}
+	});
 
-  // Open the DevTools if run with "npm start dev"
-  if (process.argv.includes("dev")) {
-    mainWindow.webContents.openDevTools();
-  }
+	if (shouldQuit) {
+		app.quit();
+		return;
+	}
 
-  // Set responders for window events.
-  mainWindow.on("closed", function() {
-    mainWindow = null;
-  });
+	// Create the browser window.
+	mainWindow = new BrowserWindow(electronOptions);
 
-  if (config.kioskmode) {
-    mainWindow.on("blur", function() {
-      mainWindow.focus();
-    });
+	// and load the index.html of the app.
+	// If config.address is not defined or is an empty string (listening on all interfaces), connect to localhost
+	var address =
+		(config.address === void 0) | (config.address === "")
+			? (config.address = "localhost")
+			: config.address;
+	mainWindow.loadURL(`http://${address}:${config.port}`);
 
-    mainWindow.on("leave-full-screen", function() {
-      mainWindow.setFullScreen(true);
-    });
+	// Open the DevTools if run with "npm start dev"
+	if (process.argv.includes("dev")) {
+		mainWindow.webContents.openDevTools();
+	}
 
-    mainWindow.on("resize", function() {
-      setTimeout(function() {
-        mainWindow.reload();
-      }, 1000);
-    });
-  }
+	mainWindow.focus();
+
+	// Set responders for window events.
+	mainWindow.on("closed", function() {
+		mainWindow = null;
+	});
+
+	if (config.kioskmode) {
+		mainWindow.on("blur", function() {
+			mainWindow.focus();
+		});
+
+		mainWindow.on("leave-full-screen", function() {
+			mainWindow.setFullScreen(true);
+		});
+
+		mainWindow.on("resize", function() {
+			setTimeout(function() {
+				mainWindow.reload();
+			}, 1000);
+		});
+	}
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 app.on("ready", function() {
-  console.log("Launching application.");
-  createWindow();
+	console.log("Launching application.");
+	createWindow();
 });
 
 // Quit when all windows are closed.
 app.on("window-all-closed", function() {
-  createWindow();
+	createWindow();
 });
 
 app.on("activate", function() {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow();
-  }
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow();
+	}
 });
 
 /* This method will be called when SIGINT is received and will call
@@ -110,23 +129,23 @@ app.on("activate", function() {
  * core.stop() is called by process.on("SIGINT"... in `app.js`
  */
 app.on("before-quit", event => {
-  console.log("Shutting down server...");
-  event.preventDefault();
-  setTimeout(() => {
-    process.exit(0);
-  }, 3000); // Force-quit after 3 seconds.
-  core.stop();
-  process.exit(0);
+	console.log("Shutting down server...");
+	event.preventDefault();
+	setTimeout(() => {
+		process.exit(0);
+	}, 3000); // Force-quit after 3 seconds.
+	core.stop();
+	process.exit(0);
 });
 
 // Start the core application if server is run on localhost
 // This starts all node helpers and starts the webserver.
 if (
-  ["localhost", "127.0.0.1", "::1", "::ffff:127.0.0.1", undefined].indexOf(
-    config.address,
-  ) > -1
+	["localhost", "127.0.0.1", "::1", "::ffff:127.0.0.1", undefined].indexOf(
+		config.address,
+	) > -1
 ) {
-  core.start(function(c) {
-    config = c;
-  });
+	core.start(function(c) {
+		config = c;
+	});
 }
--- modules/MMM-Jira/MMM-Jira.js
+++ modules/MMM-Jira/MMM-Jira.js
@@ -1,201 +1,207 @@
 /* global Module */
 Module.register("MMM-Jira", {
-    defaults: {
-        width: 1200,
-        height: 600,
-        chartLineColor: "rgb(45, 41, 38)",
-        chartAreaColor: "rgba(201, 226, 224, 1)",
-        updateInterval: 1000 * 60,
-        dataUpdateInterval: 1000 * 60 * 60,
-        viewRotation: ["month", "week", "status"],
-    },
+	// Load required additional scripts
+	defaults: {
+		width: 1200,
+		height: 600,
+		chartLineColor: "rgb(45, 41, 38)",
+		chartAreaColor: "rgba(201, 226, 224, 1)",
+		updateInterval: 1000 * 60,
+		dataUpdateInterval: 1000 * 60 * 60,
+		viewRotation: ["month", "week", "status"],
+	},
 
-    // Load required additional scripts
-    getScripts: function () {
-        return [
-            "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js",
-        ];
-    },
+	getScripts: function() {
+		return [
+			"https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js",
+		];
+	},
 
-    start: function () {
-        this.jira_data = "NO_JIRA_DATA";
-        this.current_view = 0;
+	start: function() {
+		this.jira_data = "NO_JIRA_DATA";
+		this.current_view = 0;
 
-        this.sendSocketNotification("JIRA_GET_DATA", this.config);
+		this.sendSocketNotification("JIRA_GET_DATA", this.config);
 
-        const self = this;
-        setInterval(function () {
-            self.sendSocketNotification("JIRA_GET_DATA", self.config);
-        }, self.config.dataUpdateInterval);
-        setInterval(function () {
-            self.updateDom();
-        }, self.config.updateInterval);
+		const self = this;
+		setInterval(function() {
+			self.sendSocketNotification("JIRA_GET_DATA", self.config);
+		}, self.config.dataUpdateInterval);
+	},
 
-    },
+	socketNotificationReceived(notification, payload) {
+		if (notification === "JIRA_DATA") {
+			this.jira_data = payload;
+			this.updateDom();
+		}
+	},
 
-    socketNotificationReceived(notification, payload) {
-        if (notification === "JIRA_DATA") {
-            this.jira_data = payload;
-            this.updateDom();
-        }
-    },
+	fetchJiraData: function() {
+		if (this.jira_data === "NO_JIRA_DATA") {
+			console.log(this.name + ": NO_JIRA_DATA");
+			return { x: [0], y: [0] };
+		}
 
-    fetchJiraData: function () {
-        if (this.jira_data === "NO_JIRA_DATA") {
-            console.log(this.name + ": NO_JIRA_DATA");
-            return {x: [0], y: [0]};
-        }
+		const monthNames = [
+			"Januar",
+			"Februar",
+			"Mars",
+			"April",
+			"Mai",
+			"Juni",
+			"Juli",
+			"August",
+			"September",
+			"Oktober",
+			"November",
+			"Desember",
+		];
 
-        const monthNames = [
-            "Januar",
-            "Februar",
-            "Mars",
-            "April",
-            "Mai",
-            "Juni",
-            "Juli",
-            "August",
-            "September",
-            "Oktober",
-            "November",
-            "Desember",
-        ];
+		const current_year = new Date().getFullYear();
+		let title = "";
 
-        const current_year = new Date().getFullYear();
-        let title = "";
+		const x = [];
+		const y = [];
 
-        const x = [];
-        const y = [];
+		const json_years = this.jira_data["status"]["years"];
 
-        const json_years = this.jira_data["status"]["years"];
+		Object.entries(json_years).forEach(([key, value]) => {
+			if (value["year"] === current_year) {
+				if (this.config.viewRotation[this.current_view] === "month") {
+					Object.entries(value["months"]).forEach(([key, value]) => {
+						x.push(monthNames[value["month"] - 1]);
+						y.push(value["month_count"]);
 
-        Object.entries(json_years).forEach(([key, value]) => {
-            if (value["year"] === current_year) {
-                switch (this.config.viewRotation[this.current_view]) {
-                    case "month":
-                        Object.entries(value["months"]).forEach(([key, value]) => {
-                            x.push(monthNames[value["month"] - 1]);
-                            y.push(value["month_count"]);
+						title = "Anbud per mnd i " + current_year;
+					});
+				} else if (
+					this.config.viewRotation[this.current_view] === "week"
+				) {
+					Object.entries(value["weeks"]).forEach(([key, value]) => {
+						x.push(value["week"]);
+						y.push(value["week_count"]);
 
-                            title = "Anbud per mnd i " + current_year;
-                        });
-                        break;
-                    case "week":
-                        Object.entries(value["weeks"]).forEach(([key, value]) => {
-                            x.push(value["week"]);
-                            y.push(value["week_count"]);
+						title = "Anbud per uke i " + current_year;
+					});
+				} else if (
+					this.config.viewRotation[this.current_view] === "status"
+				) {
+					Object.entries(value["issue_statuses"]).forEach(
+						([key, value]) => {
+							x.push(value["status"]);
+							y.push(value["status_count"]);
 
-                            title = "Anbud per uke i " + current_year;
-                        });
-                        break;
-                    case "status":
-                        Object.entries(value["issue_statuses"]).forEach(
-                            ([key, value]) => {
-                                x.push(value["status"]);
-                                y.push(value["status_count"]);
+							title = "Status på anbud i " + current_year;
+						},
+					);
+				}
 
-                                title = "Status på anbud i " + current_year;
-                            },
-                        );
-                        break;
-                }
-            }
-        });
+				this.current_view++;
+				if (this.current_view >= this.config.viewRotation.length) {
+					this.current_view = 0;
+				}
+			}
+		});
 
-        return {x: x, y: y, title: title};
-    },
+		return { x: x, y: y, title: title };
+	},
 
-    generateChart: function (canvas, chartData) {
-        if (chartData) {
-            new Chart(canvas, {
-                type: 'bar',
-                data: {
-                    labels: chartData.x,
-                    datasets: [{
-                        label: "",
-                        fill: true,
-                        data: chartData.y,
-                        borderColor: this.config.chartLineColor,
-                        backgroundColor: this.config.chartAreaColor,
-                        pointRadius: 0,
-                    }]
-                },
-                options: {
-                    responsive: false,
-                    title: {
-                        display: true,
-                        text: chartData.title,
-                        fontColor: "white",
-                        fontSize: 24,
-                        fontFamily: "'Raleway', sans-serif"
-                    },
-                    scales: {
-                        xAxes: [{
-                            display: true,
-                            ticks: {
-                                fontColor: 'white',
-                                fontSize: 22,
-                                fontFamily: "'Raleway', sans-serif",
-                                maxRotation: 90,
-                                minRotation: 90
-                            },
-                            gridLines: {
-                                display: false,
-                            },
-                        }],
-                        yAxes: [{
-                            display: false,
-                            ticks: {
-                                beginAtZero: true
-                            },
-                            gridLines: {
-                                display: false,
-                            },
-                        }]
-                    },
-                    layout: {
-                        padding: {
-                            left: 10,
-                            right: 10,
-                            top: 10,
-                            bottom: 10
-                        }
-                    },
-                    legend: {
-                        display: false,
-                    },
-                    plugins: {
-                        datalabels: {
-                            anchor: "end",
-                            align: "top",
-                            borderRadius: 4,
-                            color: 'white',
-                            display: () => true,
-                            formatter: (value) => parseFloat(value).toFixed(0),
-                            font: {
-                                size: 22,
-                                family: "'Raleway', sans-serif"
-                            },
-                        }
-                    },
-                }
-            });
-        }
-    },
-    // Override dom generator.
-    getDom: function () {
-        const wrapper = document.createElement("canvas");
-        wrapper.width = this.config.width;
-        wrapper.height = this.config.height;
+	generateChart: function(canvas, chartData) {
+		if (chartData) {
+			new Chart(canvas, {
+				type: "bar",
+				data: {
+					labels: chartData.x,
+					datasets: [
+						{
+							label: "",
+							fill: true,
+							data: chartData.y,
+							borderColor: this.config.chartLineColor,
+							backgroundColor: this.config.chartAreaColor,
+							pointRadius: 0,
+						},
+					],
+				},
+				options: {
+					responsive: false,
+					title: {
+						display: true,
+						text: chartData.title,
+						fontColor: "white",
+						fontSize: 24,
+						fontFamily: "'Raleway', sans-serif",
+					},
+					scales: {
+						xAxes: [
+							{
+								display: true,
+								ticks: {
+									fontColor: "white",
+									fontSize: 22,
+									fontFamily: "'Raleway', sans-serif",
+									maxRotation: 90,
+									minRotation: 90,
+								},
+								gridLines: {
+									display: false,
+								},
+							},
+						],
+						yAxes: [
+							{
+								display: false,
+								ticks: {
+									beginAtZero: true,
+								},
+								gridLines: {
+									display: false,
+								},
+							},
+						],
+					},
+					layout: {
+						padding: {
+							left: 10,
+							right: 10,
+							top: 10,
+							bottom: 10,
+						},
+					},
+					legend: {
+						display: false,
+					},
+					plugins: {
+						datalabels: {
+							anchor: "end",
+							align: "top",
+							borderRadius: 4,
+							color: "white",
+							display: () => true,
+							formatter: value => parseFloat(value).toFixed(0),
+							font: {
+								size: 22,
+								family: "'Raleway', sans-serif",
+							},
+						},
+					},
+				},
+			});
+		}
+	},
+	// Override dom generator.
+	getDom: function() {
+		const wrapper = document.createElement("canvas");
+		wrapper.id = "chart";
+		wrapper.width = this.config.width;
+		wrapper.height = this.config.height;
 
-        wrapper.style.width = "inherit";
-        wrapper.style.height = "inherit";
-        const ctx = wrapper.getContext("2d");
-        const data = this.fetchJiraData();
-        this.generateChart(ctx, data);
+		wrapper.style.width = "inherit";
+		wrapper.style.height = "inherit";
+		const ctx = wrapper.getContext("2d");
+		const data = this.fetchJiraData();
+		this.generateChart(ctx, data);
 
-        if ((this.current_view++) >= this.config.viewRotation.length - 1) this.current_view = 0;
-
-        return wrapper;
-    },
+		return wrapper;
+	},
 });
